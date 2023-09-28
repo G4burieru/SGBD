07579f07972ae8b42ff3911373854df18c148afb
G4burieru <joao.gabriel.silva.santos@academico.ufpb.br>
2023-09-28 11:44:37 -0300

    inserir e exibir todos adicionados

--- teste.py
+++ teste.py
@@ -19,18 +19,70 @@ if conexao.is_connected():
 
     # Defina a consulta SQL para criar uma tabela (substitua os campos e tipos de dados conforme necessário)
     criar_tabela_sql = """
-    CREATE TABLE IF NOT EXISTS HistoricoDeVenda (
-        CPF INT PRIMARY KEY,
-        quantidade INT NOT NULL
+    CREATE TABLE IF NOT EXISTS Pessoa (
+        CPF BIGINT PRIMARY KEY,
+        email VARCHAR(255) NOT NULL,
+        nome VARCHAR(255) NOT NULL,
+        endereco VARCHAR(255) NOT NULL,
+        tipo_pessoa VARCHAR(255) NOT NULL,
+        data_nascimento DATE NOT NULL
     )
     """
+    
+    deletar_tabela_sql = """
+    DROP TABLE Pessoa
+    """
 
+    # Execute a consulta SQL para deletar a tabela
+    # cursor.execute(deletar_tabela_sql)
+    # print("Tabela deletada com sucesso")
+    
     # Execute a consulta SQL para criar a tabela
-    cursor.execute(criar_tabela_sql)
-    print("Tabela criada com sucesso.")
+    # cursor.execute(criar_tabela_sql)
+    # print("Tabela criada com sucesso.")
 
-    # Feche o cursor e a conexão
-    cursor.close()
-    conexao.close()
 else:
     print("Não foi possível conectar ao banco de dados.")
+    
+
+cpf = ""
+email = ""
+nome = ""
+endereco = ""
+tipo_pessoa = ""
+data_nascimento = ["","",""]
+
+def inserir_pessoa():
+    cpf = input("Insira o CPF:\n")
+    email = input("Insira o email:\n")
+    nome = input("Insira o nome:\n")
+    endereco = input("Insira o endereco:\n")
+    tipo_pessoa = input("Insira o tipo de pessoa:\n")
+    data_nascimento[2] = input("Insira o dia da data de nascimento:\n")
+    data_nascimento[1] = input("Insira o mes da data de nascimento:\n")
+    data_nascimento[0] = input("Insira o ano da data de nascimento:\n")
+
+    inserir_dado = "INSERT INTO Pessoa(CPF, email, nome, endereco, tipo_pessoa, data_nascimento) VALUES("+str(cpf)+","+"'"+email+"'"+","+"'"+nome+"'"+","+"'"+endereco+"'"+","+"'"+tipo_pessoa+"'"+","+"'"+str(data_nascimento[0])+"-"+str(data_nascimento[1])+"-"+str(data_nascimento[2])+"')"
+
+    cursor.execute(inserir_dado)
+
+def exibir_todos():
+    consulta_sql = "SELECT * FROM Pessoa"
+    cursor.execute(consulta_sql)
+    linhas = cursor.fetchall()
+    print("Número total de registros retornados: ", cursor.rowcount)
+
+    for linha in linhas:
+        print("cpf:", linha[0])
+        print("email:", linha[1])
+        print("nome:", linha[2])
+        print("endereco:", linha[3])
+        print("tipo:", linha[4])
+        print("data:", linha[5])
+        
+inserir_pessoa()
+exibir_todos()
+    
+cursor.close()
+conexao.commit() #LEMBRAR DE VER ESSE COMMIT PQ SO ENVIA PRA O BANCO AS COISAS COM ELE, TALVEZ SEJA INTERESSANTE COLOCAR EM TODA FUNCAO
+conexao.close()
